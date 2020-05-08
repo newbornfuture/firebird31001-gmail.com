18d1aca827485114af6b111a0ccff2146667c8cc
newbornfuture <firebird31001@gmail.com>
2020-05-08 15:26:32 +0300

    tesr commit

--- vars/allPipeline.groovy
+++ vars/allPipeline.groovy
@@ -1,18 +1,32 @@
+def call(body) {
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
 pipeline {
     agent any
-        stages {
-            stage('Build') {
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Test2') {
+            steps {
+                echo 'Testing2..'
+            }
+        }
+        stage('Deploy') {
+            steps {
                 build()
             }
-            stage('Test') {
-                test()
-            }
-            stage('Test2') {
-                test()
-            }
-            stage('Deploy') {
-                deploy()
-            }
+        }
     }
 }
-
